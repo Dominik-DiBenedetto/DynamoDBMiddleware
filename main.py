408cdf8aded04a0e8c9c01647b9ca6b83f4fbee0
Dom <thescriptinglegend@gmail.com>
2025-06-19 21:40:16 -0400

    updated get

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ table = dynamodb.Table('Pet_System_Test')
 def root():
     return {"message": "API is up!"}
 
-@app.get("/get_pet_data/{pet_id}")
-def get_player_data(pet_name: str):
-    response = table.get_item(Key={'pet_name': pet_name})
+@app.get("/get_pet_data/{pet_name}/{pet_id}")
+def get_player_data(pet_name: str, pet_id: str):
+    response = table.get_item(Key={'pet_name': pet_name, 'pet_id': pet_id})
     return response.get('Item', {"error": "Not found"})
 
 @app.post("/set_pet_data")  # ← MUST be POST!
